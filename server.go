a637afb158b947b98c85015f72c1133e061a0e54
Walidnurudin <walidnurudin47@gmail.com>
2020-12-19 20:01:42 +0700

    add template engine

--- server.go
+++ server.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Selamat datang di go lang")
+		var data = map[string]string{
+			"Name": "Walid nurudin",
+		}
+
+		var te, err = template.ParseFiles("template.html")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		// Execute() akan membuat hasil parsing template ditampilkan ke layar web browser.
+		te.Execute(w, data)
 	})
 
 	fmt.Println("starting web server at http://localhost:8080")
